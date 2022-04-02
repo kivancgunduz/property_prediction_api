dfc5b2ef2b3514de8453ff8664f4511b80767875
kivancgunduz <kivanc.gunduz35@gmail.com>
2022-04-02 13:42:08 +0200

    updated Api methods.

--- predict/prediction.py
+++ predict/prediction.py
@@ -1,11 +1,30 @@
+import pickle
+from flask import jsonify
+import pandas as pd
 
 
 class Prediction:
     """
-
+    A class that generate the prediction from model.
     """
     def __init__(self) -> None:
+        """
+        A function that initialize the class.
+        """
         pass
     
-    def predict():
-        pass
+    def predict(self, ready_df: pd.DataFrame) -> dict:
+        """
+        A function that predict the price.
+        
+        :param ready_df: A dataframe that contains the ready data.
+        :return: A dictionary that contains the prediction.
+        """
+        try:
+            model = pickle.load(open('./model/model.pkl', 'rb'))
+            prediction_price = int(model.predict(self.df)[0])
+        except Exception as e:
+            print(e)
+            return jsonify({'error': e}), 500
+        else:
+            return jsonify({'message': 'The price is: {}'.format(prediction_price)})
